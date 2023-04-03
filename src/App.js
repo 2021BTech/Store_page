bbbc49b2e708f3c481e8546f71230a9a0b255809
2021BTech <daffydanni@gmail.com>
2023-04-03 17:20:36 +0100

    footer, contact & con.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./components/Home/Home"
 import About from './components/About/About';
 import Work from "./components/Works/Work"
 import Testimonail from './components/Testimonail/Testimonail';
+import Contact from "./components/Contact/Contact"
+import Footer from "./components/Footer/Footer"
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
        <About />
        <Work/>
        <Testimonail/>
+       <Contact/>
+       <Footer />
     </div>
   );
 }
